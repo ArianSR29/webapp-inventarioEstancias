384d0df6f44d03cb2d536c958181cb3ceea45a1b
Rodrigo Arenas <arenasgonzalezrodrigo@gmail.com>
2019-01-25 23:30:23 -0600

    style: creando maquetación de login
     - Cambiando a controlador principal del login
     - Maqutando estructura del sistema de logueo
     - Generando layouts a utilizar (header, footer, dasboard, aside)
     - Creando el __confing.scss
     - Agregando la libreria de Material Desing Lite
     - Generando archivo para estilos de login
    
    Author: Rodrigo Arenas <arenasgonzalezrodrigo@gmail.com>

---
+++ application/views/admin/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login : Control de inventario</title>
+
+    <link rel="stylesheet" href=" <?php echo base_url(); ?>assets/css/__config.css">
+    <link rel="stylesheet" href=" <?php echo base_url(); ?>assets/css/login.css">
+
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/libs/mdl/material.css">
+    <script src="<?php echo base_url(); ?>assets/libs/mdl/material.js"></script>
+
+</head>
+<body>
+    <div class="main-container">
+        <div class="c-login">
+            <form action="">
+                <img src=" <?php echo base_url(); ?>/assets/img/login/login-image.png " alt="login-image">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input class="mdl-textfield__input" type="text" id="tb-user">
+                    <label class="mdl-textfield__label" for="tb-user">Usuario</label>
+                </div>
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input class="mdl-textfield__input" type="text" id="tb-password">
+                    <label class="mdl-textfield__label" for="tb-password">Contraseña</label>
+                </div>
+                <button>Entrar</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ application/views/layouts/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistema de control de inventario</title>
+
+
+    <link rel="stylesheet" href=" <?php echo base_url(); ?>/assets/css/__config.css">
+
+
+</head>
+<body>
+
+<h1>this is header</h1>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
