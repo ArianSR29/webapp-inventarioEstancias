0bb0210d462d5330a9bd9cfa60e99b824a8c968a
Rodrigo Arenas <arenasgonzalezrodrigo@gmail.com>
2019-02-13 21:19:12 -0600

    feat_ quitando modulos inecesarios y exportando nueba base de datos

--- application/views/layouts/aside.php
+++ application/views/layouts/aside.php
@@ -2,13 +2,13 @@
     <div class="c-title">
         <span>Navegación principal</span>
     </div>
-    
+
     <div class="c-listas">
         <ul>
             <li><a href="<?php echo base_url(); ?>"> <i class="fas fa-home"></i> Inicio</a></li>
             <li><a href=" <?php echo base_url(); ?>mantenimiento/consumibles  "> <i class="fas fa-paperclip"></i> Consumibles</a></li>
-            <li><a href="http://"> <i class="far fa-building"></i> Departamentos u Oficinas</a></li>     
-            <li><a href="http://"> <i class="fa fa-print"></i> Reportes</a></li>
+            <!-- <li><a href="http://"> <i class="far fa-building"></i> Departamentos u Oficinas</a></li>
+            <li><a href="http://"> <i class="fa fa-print"></i> Reportes</a></li> -->
         </ul>
     </div>
 
--- dumps/inventario_db.sql
+++ dumps/inventario_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 01-02-2019 a las 05:54:08
+-- Tiempo de generación: 14-02-2019 a las 03:17:40
 -- Versión del servidor: 5.7.24
 -- Versión de PHP: 7.2.11
 
@@ -25,14 +25,102 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tb_rol_usuario`
+-- Estructura de tabla para la tabla `tb_consumibles`
 --
 
-DROP TABLE IF EXISTS `tb_rol_usuario`;
-CREATE TABLE `tb_rol_usuario` (
-  `id_rol` int(11) NOT NULL,
-  `tipo_cuenta` varchar(10) NOT NULL,
-  `decripcion` varchar(30) NOT NULL
+DROP TABLE IF EXISTS `tb_consumibles`;
+CREATE TABLE `tb_consumibles` (
+  `id_consumible` int(10) NOT NULL,
+  `tipo` varchar(20) DEFAULT NULL,
+  `marca` varchar(20) DEFAULT NULL,
+  `descripcion` varchar(50) DEFAULT NULL,
+  `color` varchar(20) DEFAULT NULL,
+  `total_consumibles` int(3) DEFAULT NULL,
+  `estado` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tb_consumibles`
+--
+
+INSERT INTO `tb_consumibles` (`id_consumible`, `tipo`, `marca`, `descripcion`, `color`, `total_consumibles`, `estado`) VALUES
+(1, 'Teclado', 'Logitech', 'Teclado USB Inalambrico', 'Negro', 9, 1),
+(2, 'Mause', 'Logitech', 'Mouse USB Inalambrico', 'Negro', 9, 1),
+(3, 'Unidad de Estado Sol', 'S/M', 'Disco Duro de estado solido de 480Gb', 'Plateado', 4, 1),
+(4, 'Memoria RAM', 'S/M', 'Memoria interna para LAPTOP de 4Gb', NULL, 4, 1),
+(5, 'Memoria RAM', 'S/M', 'Memoria interna para PC de 2Gb', NULL, 4, 1),
+(6, 'Monitor', 'Ghia', 'Monitor de 19\"', 'Negro', 6, 1),
+(7, 'Monitor', 'Vorago', 'Monitor de 19', 'Negro', 6, 1),
+(8, 'Diadema', 'S/M', 'S/C', 'Negro', 3, 1),
+(9, 'Teclado', 'Logitech', 'Teclado con cable USB', 'Negro', 3, 1),
+(10, 'Microfono', 'S/M', 'S/C', 'Negro', 3, 1),
+(11, 'Tester 8', 'Tester 9', 'Tester 10', 'Tester 11', 33, 0),
+(12, '', '', '', '', 0, 0),
+(13, '', '', '', '', 0, 0),
+(14, 'Monitor', 'HP', 'Monitor 19', 'Negro', 30, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_consu_depar`
+--
+
+DROP TABLE IF EXISTS `tb_consu_depar`;
+CREATE TABLE `tb_consu_depar` (
+  `id_consu_depar` int(10) NOT NULL,
+  `fk_consumible` int(10) DEFAULT NULL,
+  `fk_departamento` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_departamento`
+--
+
+DROP TABLE IF EXISTS `tb_departamento`;
+CREATE TABLE `tb_departamento` (
+  `id_departamento` int(10) NOT NULL,
+  `nombre_departamento` varchar(50) DEFAULT NULL,
+  `fk_ubicacion` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_depar_encar`
+--
+
+DROP TABLE IF EXISTS `tb_depar_encar`;
+CREATE TABLE `tb_depar_encar` (
+  `id_depar_encar` int(10) NOT NULL,
+  `fk_departamentoEnc` int(10) DEFAULT NULL,
+  `fk_encargado` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_encargado`
+--
+
+DROP TABLE IF EXISTS `tb_encargado`;
+CREATE TABLE `tb_encargado` (
+  `id_encargado` int(10) NOT NULL,
+  `nombre_encargado` varchar(20) DEFAULT NULL,
+  `apellidos_encargado` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_ubicacion`
+--
+
+DROP TABLE IF EXISTS `tb_ubicacion`;
+CREATE TABLE `tb_ubicacion` (
+  `id_ubicacion` int(10) NOT NULL,
+  `ubicacion` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -43,15 +131,15 @@ CREATE TABLE `tb_rol_usuario` (
 
 DROP TABLE IF EXISTS `tb_usuarios`;
 CREATE TABLE `tb_usuarios` (
-  `id_usuario` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `apellidos` varchar(45) NOT NULL,
-  `telefono` varchar(15) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `username` varchar(15) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `estado` int(11) NOT NULL,
-  `rol` varchar(10) NOT NULL
+  `id_usuario` int(10) NOT NULL,
+  `nombre` varchar(25) DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  `telefono` varchar(12) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT NULL,
+  `rol` char(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -59,36 +147,129 @@ CREATE TABLE `tb_usuarios` (
 --
 
 INSERT INTO `tb_usuarios` (`id_usuario`, `nombre`, `apellidos`, `telefono`, `email`, `username`, `password`, `estado`, `rol`) VALUES
-(1, 'Rodrigo', 'Arenas González', '5560072127', 'arenasgonzalezrodrigo@gmail.com', 'RodrigoAn5', 'da5fcb6c28d91fa10d80ac559e7e0cb8', 1, 'SU'),
-(2, 'root', 'root', '123456', 'root@root.com', 'superUser', '0baea2f0ae20150db78f58cddac442a9', 1, 'SU');
+(1, 'Rodrigo', 'Arenas González', '5560072127', 'arenasgonzalezrodrigo@gmail.com', 'RodrigoAn5', 'da5fcb6c28d91fa10d80ac559e7e0cb8', 1, '1'),
+(7, 'Administrator', 'Administrator', '', 'Administrator', 'Administrator', '200CEB26807D6BF99FD6F4F0D1CA54D4', 1, '1');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `tb_rol_usuario`
+-- Indices de la tabla `tb_consumibles`
 --
-ALTER TABLE `tb_rol_usuario`
-  ADD PRIMARY KEY (`id_rol`);
+ALTER TABLE `tb_consumibles`
+  ADD PRIMARY KEY (`id_consumible`);
+
+--
+-- Indices de la tabla `tb_consu_depar`
+--
+ALTER TABLE `tb_consu_depar`
+  ADD PRIMARY KEY (`id_consu_depar`),
+  ADD KEY `fk_consumible` (`fk_consumible`),
+  ADD KEY `fk_departamento` (`fk_departamento`);
+
+--
+-- Indices de la tabla `tb_departamento`
+--
+ALTER TABLE `tb_departamento`
+  ADD PRIMARY KEY (`id_departamento`),
+  ADD KEY `fk_ubicacion` (`fk_ubicacion`);
+
+--
+-- Indices de la tabla `tb_depar_encar`
+--
+ALTER TABLE `tb_depar_encar`
+  ADD PRIMARY KEY (`id_depar_encar`),
+  ADD KEY `fk_encargado` (`fk_encargado`),
+  ADD KEY `fk_departamentoEnc` (`fk_departamentoEnc`);
+
+--
+-- Indices de la tabla `tb_encargado`
+--
+ALTER TABLE `tb_encargado`
+  ADD PRIMARY KEY (`id_encargado`);
+
+--
+-- Indices de la tabla `tb_ubicacion`
+--
+ALTER TABLE `tb_ubicacion`
+  ADD PRIMARY KEY (`id_ubicacion`);
 
 --
 -- Indices de la tabla `tb_usuarios`
 --
 ALTER TABLE `tb_usuarios`
-  ADD PRIMARY KEY (`id_usuario`),
-  ADD KEY `fk_rol` (`rol`);
-ALTER TABLE `tb_usuarios` ADD FULLTEXT KEY `rol` (`rol`);
+  ADD PRIMARY KEY (`id_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `tb_consumibles`
+--
+ALTER TABLE `tb_consumibles`
+  MODIFY `id_consumible` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `tb_consu_depar`
+--
+ALTER TABLE `tb_consu_depar`
+  MODIFY `id_consu_depar` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tb_departamento`
+--
+ALTER TABLE `tb_departamento`
+  MODIFY `id_departamento` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tb_depar_encar`
+--
+ALTER TABLE `tb_depar_encar`
+  MODIFY `id_depar_encar` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tb_encargado`
+--
+ALTER TABLE `tb_encargado`
+  MODIFY `id_encargado` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tb_ubicacion`
+--
+ALTER TABLE `tb_ubicacion`
+  MODIFY `id_ubicacion` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `tb_usuarios`
 --
 ALTER TABLE `tb_usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_usuario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tb_consu_depar`
+--
+ALTER TABLE `tb_consu_depar`
+  ADD CONSTRAINT `fk_consumible` FOREIGN KEY (`fk_consumible`) REFERENCES `tb_consumibles` (`id_consumible`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_departamento` FOREIGN KEY (`fk_departamento`) REFERENCES `tb_departamento` (`id_departamento`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tb_departamento`
+--
+ALTER TABLE `tb_departamento`
+  ADD CONSTRAINT `fk_ubicacion` FOREIGN KEY (`fk_ubicacion`) REFERENCES `tb_ubicacion` (`id_ubicacion`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tb_depar_encar`
+--
+ALTER TABLE `tb_depar_encar`
+  ADD CONSTRAINT `fk_departamentoEnc` FOREIGN KEY (`fk_departamentoEnc`) REFERENCES `tb_departamento` (`id_departamento`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_encargado` FOREIGN KEY (`fk_encargado`) REFERENCES `tb_encargado` (`id_encargado`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
